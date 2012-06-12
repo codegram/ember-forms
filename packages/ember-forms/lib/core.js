c600b045961baef60babb7775f231c41ab3b4068
Josep Jaume <josepjaume@gmail.com>
2012-06-12 20:49:16 +0200

    Document core

--- packages/ember-forms/lib/core.js
+++ packages/ember-forms/lib/core.js
@@ -1,4 +1,16 @@
+/**
+  Default namespace. Here's where you'll find everything related
+  to ember-forms.
+*/
 window.EF = Ember.Namespace.create({
+
+  /**
+    Will find the container form recursively through the view hierarchy. Since
+    forms cannot contain other forms (http://www.w3.org/TR/xhtml1/#prohibitions)
+    this will resolve to a single EF.Form or undefined otherwise.
+
+    @type EF.Form
+  */
   findFormRecursively: function(view){
     var currentView = view;
     do{
@@ -6,6 +18,12 @@ window.EF = Ember.Namespace.create({
     }while(currentView = view.get('parentView'));
   },
 
+  /**
+    Will find the first EF.BaseField in line looking recursively through the
+    view hierarchy.
+
+    @type EF.BaseField
+  */
   findFieldRecursively: function(view){
     var currentView = view;
     do{
@@ -13,6 +31,12 @@ window.EF = Ember.Namespace.create({
     }while(currentView = view.get('parentView'));
   },
 
+  /**
+    Returns a field class given a particular name. For example, 
+    `findField("text")` will return `EF.TextField`.
+
+    @type String
+  */
   findField: function(name){
     name = name || 'text';
     var fieldName = Ember.String.camelize(name);
