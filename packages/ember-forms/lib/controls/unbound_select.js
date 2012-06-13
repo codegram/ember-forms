ca67c8d446423d8411342822658da95927860350
Josep Jaume <josepjaume@gmail.com>
2012-06-13 20:31:47 +0200

    Add docs

--- packages/ember-forms/lib/controls/unbound_select.js
+++ packages/ember-forms/lib/controls/unbound_select.js
@@ -1,9 +1,24 @@
 var fmt = Ember.String.fmt;
 
+/**
+  `Ember.Select` is meant to be bound to collections with a changing nature, 
+  but with big collections, it comes with a big performance penalty. In order
+  to address this issue, we've created a "static" one - meaning that won't
+  change as the associated collection changes. This makes it perfect for things
+  like date selectors, gender, numerical...
+
+  The collection can be an array of values, or an array of Javascript objects
+  with `value` and `label` keys.
+*/
 EF.UnboundSelect = Ember.View.extend({
   tagName: 'select',
   template: Ember.Handlebars.compile("{{unbound view.options}}"),
 
+  /**
+    @private
+
+    Renders the options from the collection.
+  */
   options: Ember.computed(function(){
     var output;
     if(!Ember.empty(this.get('prompt'))){
