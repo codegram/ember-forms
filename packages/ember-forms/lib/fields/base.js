0bd0ec5846b0edaf9159300b662c2dcbca4b383f
Josep Jaume <josepjaume@gmail.com>
2012-07-14 18:58:01 +0200

    Remove debug code

--- packages/ember-forms/lib/fields/base.js
+++ packages/ember-forms/lib/fields/base.js
@@ -29,12 +29,6 @@ EF.BaseField = Ember.ContainerView.extend({
     var path = 'content.' + name;
     var value = this.getPath(path);
     this.set('value', this.getPath(path));
-    var content = this.get('content');
-    if(content){
-      content.addObserver(this.get('name'), this, function(){
-        console.log(this.getPath('content.name'));
-      });
-    }
   },
 
   data: Ember.computed(function(){
